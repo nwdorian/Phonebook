2832515e5a49bc97fa0d68749e87e8df778f9f87
nwdorian <neuwirthdorian@gmail.com>
2024-05-28 20:03:50 +0200

    add menu implementation

---
+++ Phonebook/Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace Phonebook;
+internal class Enums
+{
+    internal enum MenuOptions
+    {
+        AddContact,
+        DeleteContact,
+        UpdateContact,
+        ViewContact,
+        ViewAllContacts,
+        Exit
+    }
+}
