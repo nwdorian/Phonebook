3e95dea335add6cd25b040507e1bad44b0944be2
nwdorian <neuwirthdorian@gmail.com>
2024-05-28 20:18:37 +0200

    add contact context

---
+++ Phonebook/ContactContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Phonebook;
+internal class ContactContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("Default"));
+    }
+}
