23bf6e28fedb342277cbb724f7147f940b0cbde2
nwdorian <neuwirthdorian@gmail.com>
2024-05-28 21:13:11 +0200

    add and run nvarcharlimit migration

--- Phonebook/Migrations/20240528185128_AddNvarcharLimit.Designer.cs
+++ Phonebook/Migrations/20240528185128_AddNvarcharLimit.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Phonebook;
 
@@ -10,9 +11,11 @@ using Phonebook;
 namespace Phonebook.Migrations
 {
     [DbContext(typeof(ContactContext))]
-    partial class ContactContextModelSnapshot : ModelSnapshot
+    [Migration("20240528185128_AddNvarcharLimit")]
+    partial class AddNvarcharLimit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
