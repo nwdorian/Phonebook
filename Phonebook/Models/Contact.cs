f8f85dbeba0e5dd08f2c3f1d1ff7773368299e4d
nwdorian <neuwirthdorian@gmail.com>
2024-05-28 20:10:15 +0200

    add contact model

---
+++ Phonebook/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Phonebook.Models;
+internal class Contact
+{
+    public int Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string? Email { get; set; }
+    [Required]
+    public string? PhoneNumber { get; set; }
+}
